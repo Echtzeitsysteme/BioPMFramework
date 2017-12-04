40d37dfee62df8b887393e0799bf30bed2d72e5b
Sascha Edwin Zander <sascha@sezander.de>
2017-12-04 15:59:17 +0100

    added some projects

---
+++ de.tu.darmstadt.es.PatternMatchingEngine.democles/src/de/tu/darmstadt/es/PatternMatchingEngine/democles/DemoclesEngine.java
@@ -0,0 +1,7 @@
+package de.tu.darmstadt.es.PatternMatchingEngine.democles;
+
+import de.tu.darmstadt.es.biochemicalSimulationFramework.patternmatchingcontroller.patternmatchingengine.PatternMatchingEngine;
+
+public class DemoclesEngine extends PatternMatchingEngine{
+
+}
---
+++ de.tu.darmstadt.es.geolibrary.OctTreeLibrary/src/de/tu/darmstadt/es/geolibrary/octTreeLibrary/OctTreeLibrary.java
@@ -0,0 +1,7 @@
+package de.tu.darmstadt.es.geolibrary.octTreeLibrary;
+
+import de.tu.darmstadt.es.biochemicalSimulationFramework.patternmatchingcontroller.geolibrary.GeoLibrary;
+
+public class OctTreeLibrary extends GeoLibrary{
+
+}
